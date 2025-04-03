f282bf6ca5b11ba260ac548009435bf5876b053d
Rhys Bryant <rhys@voltprojects.com>
2025-04-03 22:49:04 +1100

    fix filename casing

--- src/utility.cpp
+++ src/utility.cpp
@@ -17,7 +17,7 @@
  *   You should have received a copy of the GNU Lesser General Public License
  *   along with SimpleHTTP.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "Utility.h"
+#include "utility.h"
 #include "string.h"
 
 int SimpleHTTP::Utility::toASCII(int value, char* buffer, int base,int size)
